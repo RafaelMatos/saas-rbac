572f8d3a35d215f1d238b8a318964bbd686897de
RafaelMatos <rafael-matos@outlook.com>
2024-07-24 17:51:38 -0300

    feat(api): add get membership route

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -19,6 +19,7 @@ export * from './models/organization'
 export * from './models/project'
 export * from './models/user'
 export * from './models/appointment'
+export * from './roles'
 
 const appAbilitiesSchema = z.union([
   projectSubject,
