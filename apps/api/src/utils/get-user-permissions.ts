559f12ecd0a0c6d77db5159ee99950f93ea6722c
RafaelMatos <rafael-matos@outlook.com>
2024-07-25 18:21:44 -0300

    feat(api): create update and shutdown organization routes

---
+++ apps/api/src/utils/get-user-permissions.ts
@@ -0,0 +1,13 @@
+import { Role } from '@prisma/client'
+import { defineAbilityFor, userSchema } from '@sq/auth'
+
+export function getUserPermissions(userId: string, role: Role) {
+  const authUser = userSchema.parse({
+    id: userId,
+    role,
+  })
+
+  const ability = defineAbilityFor(authUser)
+
+  return ability
+}
